26191bebcd81418286f1aee9d52f67cd64edded5
Julia <>
2023-08-16 08:56:05 +0200

    added router

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
+  import { Router, Route, Link } from "svelte-routing";
+  import Home from "./routes/home.svelte";
+  import Print from "./routes/print.svelte";
   import Header from "./lib/Header.svelte";
   import Intro from "./lib/Intro/Intro.svelte";
   import StatesSelect from "./lib/StatesSelect/StatesSelect.svelte";
   import Main from "./lib/Main/Main.svelte";
   import data from "./assets/data/metricData.json";
   import { stateClicked } from "./store/store";
+
+  export let url = "";
 </script>
 
 <Header />
@@ -15,6 +20,17 @@
     <Main {data} />
   {/if}
 </div>
+
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="print">Print</Link>
+  </nav>
+  <div>
+    <Route path="/"><Home /></Route>
+    <Route path="print" component={Print} />
+  </div>
+</Router>
 
 <style>
 </style>
